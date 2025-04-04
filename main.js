538a2214b893bf2be9e3a37f0b5e68670990b1f4
Dirceu <dirceu.ferniman.filho@escola.pr.gov.br>
2025-04-04 12:00:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
 const abas = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -11,6 +12,15 @@ for (let i = 0; i < botoes.length; i++) {
 
         // Adiciona a classe "ativo" apenas ao botão e à aba correspondente
         botoes[i].classList.add("ativo");
+
+        textos[i].classList.add("ativo");
+        textos[j].classList.remove("ativo");
+
         abas[i].classList.add("ativo");
     };
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
